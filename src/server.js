1118b7c3744515293c36034317395c3188739174
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-22 12:08:29 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { YanzGPT } = require('./yanzgpt');
+
 
 const app = express();
 const port = 3000;
